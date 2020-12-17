50c64588f0ea6c0718ed691fdf42acfa778c6a6e
Hiep Thanh Nguyen <hieptnguyen94@gmail.com>
2020-12-17 18:11:34 +0700

    Add useConfirm hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as ConfirmProvider } from './ConfirmProvider';
 export { default as ConfirmContext } from './ConfirmContext';
+export { default as useConfirm } from './useConfirm';
 export * from './types';
