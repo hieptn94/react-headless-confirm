32bfae9242aeac9dbc7194d95e1bce261477bf7c
Hiep Thanh Nguyen <hieptnguyen94@gmail.com>
2020-12-19 20:51:14 +0700

    fix: update typings

--- src/useConfirm.ts
+++ src/useConfirm.ts
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { ConfirmContextValue } from './types';
-import ConfirmContext from './ConfirmContext';
+import ConfirmContext, { ConfirmContextValue } from './ConfirmContext';
 
 export default function useConfirm(): ConfirmContextValue {
   return React.useContext(ConfirmContext);
